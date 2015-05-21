c866cf056e24f6b1d52a857bb0c5006529316fda
Esther Greatbanks <esthergreatbanks@gmail.com>
2015-05-21 16:29:57 +1200

    Add pretty spec printing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,14 @@ require 'capybara/rspec'
 
 RSpec.configure do |config|
   config.include Rack::Test::Methods
+  # Use color in STDOUT
+  config.color = true
+
+  # Use color not only in STDOUT but also in pagers and files
+  config.tty = true
+
+  # Use the specified formatter
+  config.formatter = :documentation # :progress, :html, :textmate
 end
 
 def app
